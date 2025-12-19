bd5699b04b583c29f3e6a36ab1500b016a4469cf
Jonathan Gonzalez Benavides <jonathangzzben@gmail.com>
2025-12-19 06:17:58 -0600

    Make ring buffer generic

--- include/arklog/ring_buffer.h
+++ include/arklog/ring_buffer.h
@@ -2,42 +2,55 @@
 #define ARKLOG_RING_BUFFER_H
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct AlogRingBuffer {
-  void **data;
+  void *data;
   size_t head;
   size_t tail;
   size_t size;
 } AlogRingBuffer;
 
 AlogRingBuffer alog_ring_buffer_create(size_t size);
-void alog_ring_buffer_push(AlogRingBuffer *ring, void *data);
-void *alog_ring_buffer_pop(AlogRingBuffer *ring);
+bool alog_ring_buffer_push(AlogRingBuffer *ring, void *data);
+bool alog_ring_buffer_pop(AlogRingBuffer *ring, void *dest);
 
 AlogRingBuffer alog_ring_buffer_create(size_t size) {
   AlogRingBuffer ring;
-  ring.data = (void **)malloc((size + 1) * sizeof(void *));
+  ring.data = (void *)malloc((size + 1) * sizeof(void *));
   ring.head = 0;
   ring.tail = 0;
   ring.size = size + 1; // One extra space for wrapping
   return ring;
 }
 
-void alog_ring_buffer_push(AlogRingBuffer *ring, void *data) {
+bool alog_ring_buffer_push(AlogRingBuffer *ring, void *data) {
+  assert(ring != NULL);
+  assert(data != NULL);
+
   const size_t new_tail = (ring->tail + 1) % ring->size;
   assert(new_tail != ring->head); // Full queue
-  ring->data[ring->tail] = data;
+  void *dest = (char *)ring->data + (ring->tail * sizeof(int));
+  memcpy(dest, data, sizeof(int));
+
   ring->tail = new_tail;
+  return true;
 }
 
-void *alog_ring_buffer_pop(AlogRingBuffer *ring) {
+bool alog_ring_buffer_pop(AlogRingBuffer *ring, void *dest) {
+  assert(ring != NULL);
   assert(ring->head != ring->tail); // Empty queue
+
   const size_t new_head = (ring->head + 1) % ring->size;
-  void *data = ring->data[ring->head];
+  // void *data = ring->data[ring->head];
+  void *data = (char *)ring->data + (ring->head * sizeof(int));
+  memcpy(dest, data, sizeof(int));
   ring->head = new_head;
-  return data;
+  return true;
 }
 
 #endif // ARKLOG_RING_BUFFER_H
--- tests/ring_buffer_tests.c
+++ tests/ring_buffer_tests.c
@@ -13,12 +13,10 @@ void test_ring_buffer(void) {
     alog_ring_buffer_push(&ring_buffer, &i);
   }
   for (int i = 0; i < 5; i++) {
-    int *data = (int *)ring_buffer.data[i];
-    printf("Element at index %d: %d\n", i, *data);
-  }
-  for (int i = 0; i < 5; i++) {
     printf("Popping %d\n", i);
-    int *data = (int *)alog_ring_buffer_pop(&ring_buffer);
-    printf("Element at index %d: %d\n", i, *data);
+    // int *data = (int *)alog_ring_buffer_pop(&ring_buffer);
+    int data;
+    alog_ring_buffer_pop(&ring_buffer, &data);
+    printf("Element at index %d: %d\n", i, data);
   }
 }
